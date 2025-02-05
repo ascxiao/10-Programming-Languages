a260ef4507d585ead7991867bba30c6cd8deb89c
Carlos Valderrama <81756261+ascxiao@users.noreply.github.com>
2025-02-05 18:44:40 +0800

    commit siguro

--- C#/MyApp/obj/Debug/net8.0/MyApp.AssemblyInfo.cs
+++ C#/MyApp/obj/Debug/net8.0/MyApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d97fa2f102cf2062d654e7c1615ab7ed3eeeeb6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
